b0519765ba781dbcb703a03e03d352d9b36a05dd
Tomaz Golob <tomaz.golob@mediately.co>
2026-01-09 21:37:19 +0100

    Initial commit

---
+++ src/test/java/com/prpo/usage/PrpoUsageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prpo.usage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrpoUsageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
